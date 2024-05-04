462ef667409a78d995673a3700a421a121eb0f9b
Nikita-Babich <nikita.babich.a@gmail.com>
2024-05-04 15:00:07 +0300

    Update reader.h

--- reader.h
+++ reader.h
@@ -12,6 +12,7 @@ void read_config(){
     
     std::string line;
     int lineCount = 0;
+    int colorr,colorg,colorb;
     while (getline(configFile, line)) {
         if (lineCount % 2 == 1) {
         	stringstream ss(line);
@@ -29,21 +30,26 @@ void read_config(){
                     ss >> light.x >> light.y >> light.z;
                     break;
                 case 9:
-                	int colorr,colorg,colorb;
+                	
                     ss >> colorr >> colorg >> colorb;
                     light.color = RGB(colorr,colorg,colorb);
                     break;
                 case 11:
-                    ss >> objectColor[0] >> objectColor[1] >> objectColor[2];
+                    
+                    ss >> colorr >> colorg >> colorb;
+                    main_color = RGB(colorr,colorg,colorb);
                     break;
                 case 13:
-                    ss >> objectDiffusion[0] >> objectDiffusion[1] >> objectDiffusion[2];
+                    ss >> colorr >> colorg >> colorb;
+                    diffusion = RGB(colorr,colorg,colorb);
                     break;
                 case 15:
-                    ss >> objectMirror[0] >> objectMirror[1] >> objectMirror[2];
+                    ss >> colorr >> colorg >> colorb;
+                    mirror = RGB(colorr,colorg,colorb);
                     break;
                 case 17:
-                    ss >> objectAmbient[0] >> objectAmbient[1] >> objectAmbient[2];
+                    ss >> colorr >> colorg >> colorb;
+                    ambience = RGB(colorr,colorg,colorb);
                     break;
             }
         }
@@ -51,7 +57,87 @@ void read_config(){
         
 }
 
+std::string filepath = "files/cube.vtk";
+void readVtkFile(const string& filepath, Allpoints& allpoints, Scene& scene) {
+    ifstream file(filepath);
+    if (!file) {
+        cerr << "Error: Unable to open file " << filepath << endl;
+        return;
+    }
+    
+    string line;
+    bool processingPoints = false;
+    bool processingFaces = false;
+    
+    std::vector<Point*> pointRefs;
+    
+    while (getline(file, line)) {
+        stringstream ss(line);
+        std::string keyword;
+        ss >> keyword;
 
+        // Skip comments and empty lines
+        if (keyword.empty() || keyword[0] == '#') continue;
+
+        if (keyword == "POINTS") {
+            processingPoints = true;
+            processingFaces = false;
+            int numPoints;
+            ss >> numPoints; 
+            string type;
+            ss >> type; // Type  float
+
+            // Read points
+            for (int i = 0; i < numPoints; i++) {
+                if (getline(file, line)) {
+                    stringstream pointStream(line);
+                    Point p;
+                    pointStream >> p.x >> p.y >> p.z;
+                    p.color = main_color; // object color
+                    allpoints.push_back(p);
+                    pointRefs.push_back(&allpoints.back()); // Store reference to point, might not be needed
+                }
+            }
+        } else if (keyword == "POLYGONS") {
+            processingPoints = false;
+            processingFaces = true;
+            int numPolygons;
+            int numVertices;
+            ss >> numPolygons >> numVertices; // Number of polygons and total vertices (ignored)
+
+            // Read faces
+            for (int i = 0; i < numPolygons; i++) {
+                if (getline(file, line)) {
+                    stringstream faceStream(line);
+                    int vertexCount; // Always 3 for a triangle face
+                    faceStream >> vertexCount;
+
+                    if (vertexCount == 3) { // check if file is correct
+                        Face face;
+                        int indexA, indexB, indexC;
+                        faceStream >> indexA >> indexB >> indexC;
+
+                        // Set pointers to points from allpoints
+                        face.A = pointRefs[indexA];
+                        face.B = pointRefs[indexB];
+                        face.C = pointRefs[indexC];
+
+                        // Add face to scene
+                        scene.push_back(face);
+
+                        // Update point facePtrs vectors, now points have pointers to face
+                        face.A->facePtrs.push_back(&scene.back());
+                        face.B->facePtrs.push_back(&scene.back());
+                        face.C->facePtrs.push_back(&scene.back());
+                    }
+                }
+            }
+        }
+    }
+
+    file.close();
+    
+}
 
 
 
